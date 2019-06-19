20c2c9561713c98f0055e4cb85285fa095c6e4a7
Chuan <orcuslc@hotmail.com>
2019-06-19 22:36:26 +0800

    add NW corner rule

---
+++ north_west_corner_rule.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def NW_corner_rule(a, b):
+    """
+    North-West Corner rule for discrete optimal transport:
+        Finding the vertex of the polytope of the feasible set U(a, b),
+        where the minimum is attained.
+
+    Reference: P44, Sec 3.4.2
+
+    @input a, b: np.ndarray, of shape (n, ) and (m, ), the source histogram and the target histogram
+    @return P: np.ndarray, of shape (n, m), one of the vertex of the polytope, P[i, j]: the flow from a[i] to b[j]
+
+    >>> a = np.array([0.2, 0.5, 0.3])
+    >>> b = np.array([0.5, 0.1, 0.4])
+    >>> NW_corner_rule(a, b)
+    array([[0.2, 0. , 0. ],
+           [0.3, 0.1, 0.1],
+           [0. , 0. , 0.3]])
+    """
+    P = np.zeros((a.shape[0], b.shape[0]))
+    i = j = 0
+    r = a[0]
+    c = b[0]
+    while i < a.shape[0] and j < b.shape[0]:
+        P[i, j] = min(r, c)
+        r -= P[i, j]
+        c -= P[i, j]
+        if r == 0:
+            i += 1
+            if i < a.shape[0]:
+                r = a[i]
+        if c == 0:
+            j += 1
+            if j < b.shape[0]:
+                c = b[j]
+    return P
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
