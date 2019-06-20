aecc2976a22238c313d65e5d1fed34fcab3880f7
Chuan <orcuslc@hotmail.com>
2019-06-20 18:35:13 +0800

    add NW corner rule

--- north_west_corner_rule.py
+++ north_west_corner_rule.py
@@ -36,6 +36,38 @@ def NW_corner_rule(a, b):
                 c = b[j]
     return P
 
+def compute_NW_solutions(a, b, sigma1, sigma2):
+    """
+    compute an arbitrary NW solution by permutation
+
+    Reference: Page 45, Section 3.4.2
+
+    @param a, b: the source histogram and the target histogram
+    @param sigma1, sigma2: the permutation vector for a and b respectively
+    @return P_permutation: the permutated NW solution by
+        P_permutation = sigma1_inv(sigma2_inv(P, column), row)
+        where P is the transport matrix of sigma1(a) and sigma2(b)
+
+    >>> a = np.array([0.2, 0.5, 0.3])
+    >>> b = np.array([0.5, 0.1, 0.4])
+    >>> sigma1 = np.array([2, 0, 1])
+    >>> sigma2 = np.array([2, 1, 0])
+    >>> compute_NW_solutions(a, b, sigma1, sigma2)
+    array([[0., 0.1, 0.1],
+        [0.5, 0., 0.],
+        [0., 0., 0.3]])
+    """
+    a = a[sigma1]
+    b = b[sigma2]
+    P = NW_corner_rule(a, b)
+    print(P)
+    sigma1_inv = np.argsort(sigma1)
+    sigma2_inv = np.argsort(sigma2)
+    P = P[:, sigma2_inv]
+    P = P[sigma1_inv, :]
+    return P
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
